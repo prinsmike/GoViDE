c7c01c9ea679a803ee8a6e93b6a72b1ffdcae9dd
Michael Prinsloo <mike.prinsloo@gmail.com>
2016-09-27 13:09:25 +0200

    Added install script.

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo -e '>> Initiating install.'
+
+echo -e '>> Checking for Docker install and pulling image.'
+if [ $(command -v docker) ]; then
+	docker pull prinsmike/govide
+else
+	echo -e 'ERROR: Docker is not installed or under bin as docker.'
+	exit 1
+fi
+
+echo -e '>> Setting up govide command, will require elevated provileges.'
+
+cat > /tmp/govide <<EOF
+#!/bin/bash
+
+if [ \$(command -v docker) ]; then
+	docker run --rm -tiv \${GOPATH}/src:/go/src prinsmike/govide
+else
+	echo -e 'This program requires Docker, please install docker'
+fi
+EOF
+
+chmod +x /tmp/govide
+
+sudo su -c "mv /tmp/govide /usr/local/bin/govide"
+
+echo -e '>> Setup complete.'
+echo -e '\n'
